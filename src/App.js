6e6f39731f2dd48779be4c1470c0a9a145c69979
David Palacio <ing.david.palacio@gmail.com>
2021-07-16 23:58:27 -0500

    Dandole mi Estilo a la Pagina

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DP Store
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vamos a Aprender Vendiendo XD !!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
