87a5aec0f4cdce50d926ba84ef2726d33a233b88
David Palacio <ing.david.palacio@gmail.com>
2021-07-23 11:30:53 -0500

    Desafio 03 - JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
+      </header>
+
+      <body className="App-body">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DP Store
-        </p>
+        <h1>
+          Bienvenidos a la Tienda de Doña Marleny, 
+        </h1>
+        <h2>Su Tienda de Barrio OnLine !!!</h2>
+        <p>Proyecto React de David Palacio</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="#"
           rel="noopener noreferrer"
         >
-          Vamos a Aprender Vendiendo XD !!!
+        Entrá Ve!!
         </a>
-      </header>
+      </body>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+//import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+
+function NavBar() {
+    return (
+        //<div>Bienvenidos</div>
+        <Navbar bg="light" expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">Tiendita de Marleny</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Dentre</Nav.Link>
+                <NavDropdown title="Vitrina" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Mercado</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Dulces</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">No Fiamos</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        
+    );
+}
+export default NavBar;
