b2c8e9727c15877de800605fc0e8efcb4884c2f5
David Palacio <ing.david.palacio@gmail.com>
2021-08-19 22:11:36 -0500

    Desafio 7

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -14,6 +14,7 @@ export default function ItemList() {
           name: "Naranjas",
           description: "Toronja",
           img:  "https://images.unsplash.com/photo-1613747495731-3add0988073a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+          price: "1500",
           stock: 9
         }, 
         {
@@ -21,6 +22,7 @@ export default function ItemList() {
             name: "Mandarinas",
             description: "Arrayana",
             img: "https://images.unsplash.com/photo-1611329646571-689ddf8bfee9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+            price: "2500",
             stock: 50
           },
           {
@@ -28,6 +30,7 @@ export default function ItemList() {
             name: "Cebollas",
             description: "Cabezona",
             img: " https://images.unsplash.com/photo-1580201092675-a0a6a6cafbb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            price: "950",
             stock: 253
           }
          
@@ -56,6 +59,7 @@ export default function ItemList() {
                     <Card.Title>{cadaProducto.name}</Card.Title>
                     <Card.Img variant="top" src={cadaProducto.img} />
                     <Card.Text>{cadaProducto.description}</Card.Text>
+                    <Card.Text>${cadaProducto.price}</Card.Text>
                     <Card.Text>Disponibles: {cadaProducto.stock}</Card.Text>
                     <div>
                     <ItemCount stock={cadaProducto.stock}/>
